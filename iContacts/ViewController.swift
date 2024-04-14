6515575fc5c34942912ac56ee9e5855ec3da9e4b
Gastronom <terlimbombom0@gmail.com>
2024-04-14 21:21:30 +0300

    Initial Commit

---
+++ iContacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iContacts
+//
+//  Created by Gastronom on 14.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
